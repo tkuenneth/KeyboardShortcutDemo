9eb3b12c959c290be16c84fa97cba20aeac28cd2
Thomas Künneth <kuennetht@gmail.com>
2025-10-04 15:03:23 +0200

    Refactoring

--- composeApp/src/androidMain/kotlin/dev/tkuenneth/keyboardshortcutdemo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/tkuenneth/keyboardshortcutdemo/MainActivity.kt
@@ -10,10 +10,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -25,28 +27,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         listKeyboardShortcutInfo = listOf(
             KeyboardShortcutInfo(
-                getString(R.string.say_hello),
-                KeyEvent.KEYCODE_H,
-                KeyEvent.META_CTRL_ON
+                getString(R.string.say_hello), KeyEvent.KEYCODE_H, KeyEvent.META_CTRL_ON
             )
         )
-        mapKeyboardShortcuts =
-            listKeyboardShortcutInfo.associateWith { shortcutInfo ->
-                KeyboardShortcut(
-                    label = shortcutInfo.label.toString(),
-                    shortcut = shortcutInfo.getDisplayString(),
-                )
-            }
+        mapKeyboardShortcuts = listKeyboardShortcutInfo.associateWith { shortcutInfo ->
+            KeyboardShortcut(
+                label = shortcutInfo.label.toString(),
+                shortcut = shortcutInfo.getDisplayString(),
+            )
+        }
         setContent {
             var snackbarMessage by remember { mutableStateOf("") }
             val listKeyboardShortcuts = remember {
                 mapKeyboardShortcuts.values.toList()
             }
-            key(listKeyboardShortcuts) {
+            val helloMessage = stringResource(R.string.hello)
+            LaunchedEffect(listKeyboardShortcuts) {
                 listKeyboardShortcuts.forEach { shortcut ->
-                    LaunchedEffect(shortcut) {
-                        shortcut.events.collect {
-                            snackbarMessage = getString(R.string.hello)
+                    launch {
+                        shortcut.events.collectLatest {
+                            snackbarMessage = helloMessage
                         }
                     }
                 }
@@ -60,24 +60,23 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onProvideKeyboardShortcuts(
-        data: MutableList<KeyboardShortcutGroup>,
-        menu: Menu?,
-        deviceId: Int
+        data: MutableList<KeyboardShortcutGroup>, menu: Menu?, deviceId: Int
     ) {
         super.onProvideKeyboardShortcuts(data, menu, deviceId)
         data.add(KeyboardShortcutGroup(getString(R.string.general), listKeyboardShortcutInfo))
     }
 
     override fun onKeyShortcut(
-        keyCode: Int,
-        event: KeyEvent
+        keyCode: Int, event: KeyEvent
     ): Boolean {
-        listKeyboardShortcutInfo.find { it.keycode == keyCode && event.hasModifiers(it.modifiers) }
-            ?.let {
-                val shortcut = mapKeyboardShortcuts[it]
-                shortcut?.triggerAction()
-                return true
-            }
+        val keyboardShortcutInfo = listKeyboardShortcutInfo.find {
+            it.keycode == keyCode && event.hasModifiers(it.modifiers)
+        }
+        val shortcut = mapKeyboardShortcuts[keyboardShortcutInfo]
+        if (shortcut != null) {
+            shortcut.triggerAction()
+            return true
+        }
         return super.onKeyShortcut(keyCode, event)
     }
 
@@ -86,10 +85,8 @@ class MainActivity : ComponentActivity() {
         if (modifiers and KeyEvent.META_CTRL_ON != 0) {
             parts.add("Ctrl")
         }
-        keycode.let { keyCode ->
-            if (keyCode in KeyEvent.KEYCODE_A..KeyEvent.KEYCODE_Z) {
-                parts.add(('A' + (keyCode - KeyEvent.KEYCODE_A)).toString())
-            }
+        if (keycode in KeyEvent.KEYCODE_A..KeyEvent.KEYCODE_Z) {
+            parts.add(('A' + (keycode - KeyEvent.KEYCODE_A)).toString())
         }
         return parts.joinToString("+")
     }
