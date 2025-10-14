2ea5abe95e722823294200b990258cef68ded3c5
Thomas Künneth <kuennetht@gmail.com>
2025-10-14 13:00:40 +0200

    Preliminary implementation for iOS

--- composeApp/src/iosMain/kotlin/dev/tkuenneth/keyboardshortcutdemo/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/dev/tkuenneth/keyboardshortcutdemo/MainViewController.kt
@@ -1,10 +1,93 @@
 package dev.tkuenneth.keyboardshortcutdemo
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ExportObjCClass
+import kotlinx.cinterop.ObjCAction
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import platform.Foundation.NSSelectorFromString
+import platform.UIKit.UIKeyCommand
+import platform.UIKit.UIKeyModifierCommand
+import platform.UIKit.UIViewAutoresizingFlexibleHeight
+import platform.UIKit.UIViewAutoresizingFlexibleWidth
+import platform.UIKit.UIViewController
+import platform.UIKit.addChildViewController
+import platform.UIKit.addKeyCommand
+import platform.UIKit.didMoveToParentViewController
 
-fun MainViewController() = ComposeUIViewController {
-    MainScreen(
-        listKeyboardShortcuts = emptyList(),
-        hardKeyboardHidden = false,
-    ) {}
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+@ExportObjCClass
+class KeyboardHostingController(
+    private val onAddAction: () -> Unit,
+    private val contentFactory: () -> UIViewController,
+) : UIViewController(nibName = null, bundle = null) {
+
+    private lateinit var composeController: UIViewController
+
+    override fun viewDidLoad() {
+        super.viewDidLoad()
+        composeController = contentFactory()
+        addChildViewController(composeController)
+        view.addSubview(composeController.view)
+        composeController.view.autoresizingMask = UIViewAutoresizingFlexibleWidth or UIViewAutoresizingFlexibleHeight
+        composeController.didMoveToParentViewController(this)
+        val cmd = UIKeyCommand.keyCommandWithInput(
+            input = "N",
+            modifierFlags = UIKeyModifierCommand,
+            action = NSSelectorFromString("handleAddCommand:")
+        )
+        cmd.discoverabilityTitle = "Add hello"
+        addKeyCommand(cmd)
+    }
+
+    override fun viewDidAppear(animated: Boolean) {
+        super.viewDidAppear(animated)
+        becomeFirstResponder()
+    }
+
+    override fun viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        composeController.view.setFrame(view.bounds)
+    }
+
+    override fun canBecomeFirstResponder(): Boolean = true
+
+    @ObjCAction
+    fun handleAddCommand(sender: UIKeyCommand?) {
+        onAddAction()
+    }
+}
+
+fun MainViewController(): UIViewController {
+    val trigger = Channel<Unit>(CONFLATED)
+    val addAction: () -> Unit = { trigger.trySend(Unit) }
+    return KeyboardHostingController(
+        onAddAction = addAction,
+        contentFactory = {
+            ComposeUIViewController {
+                var hardHidden by remember { mutableStateOf(false) }
+                LaunchedEffect(Unit) {
+                    launch {
+                        trigger.receiveAsFlow().collectLatest {
+                            hardHidden = !hardHidden
+                        }
+                    }
+                }
+                MainScreen(
+                    listKeyboardShortcuts = emptyList(),
+                    hardKeyboardHidden = hardHidden,
+                ) { addAction() }
+            }
+        }
+    )
 }
--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,6 +2,38 @@ import UIKit
 import SwiftUI
 import ComposeApp
 
+final class KeyCommandsHost: UIViewController {
+    private let childController: UIViewController
+
+    init(child: UIViewController) {
+        self.childController = child
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild(childController)
+        view.addSubview(childController.view)
+        childController.view.frame = view.bounds
+        childController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        childController.didMove(toParent: self)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        becomeFirstResponder()
+    }
+
+    override var canBecomeFirstResponder: Bool { true }
+
+    @objc func handleAddCommand(_ sender: UIKeyCommand?) {
+        _ = childController.perform(NSSelectorFromString("handleAddCommand:"), with: nil)
+    }
+}
+
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         MainViewControllerKt.MainViewController()
@@ -16,6 +48,3 @@ struct ContentView: View {
             .ignoresSafeArea()
     }
 }
-
-
-
