0558b66e6fa5be5a04eab1616a4cc3a8e479f398
Thomas Künneth <kuennetht@gmail.com>
2025-10-19 10:22:31 +0200

    Make sure that Gboard does not swallow our shortcuts

--- composeApp/src/androidMain/kotlin/dev/tkuenneth/keyboardshortcutdemo/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/dev/tkuenneth/keyboardshortcutdemo/Platform.android.kt
@@ -5,14 +5,13 @@ import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.isAltPressed
 import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.onInterceptKeyBeforeSoftKeyboard
 import androidx.compose.ui.input.key.type
 
 actual fun Modifier.keyboardShortcuts(
     vararg shortcuts: Pair<Key, () -> Unit>
 ): Modifier = then(
-    Modifier.onPreviewKeyEvent { event ->
-        println("onPreKeyEvent: $event")
+    Modifier.onInterceptKeyBeforeSoftKeyboard() { event ->
         if (event.type == KeyEventType.KeyDown) {
             shortcuts.forEach { (key, action) ->
                 if (event.key == key && event.isAltPressed) {
